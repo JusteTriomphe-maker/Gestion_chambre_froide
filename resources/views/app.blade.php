cd694553b80d1fef588fecad4feaebfd4d9ecbec
Juste Triomphe <justetriomphema19@gmail.com>
2026-06-04 23:44:01 +0200

    fix: bloquer traduction navigateur qui casse React (insertBefore error)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" translate="no">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="google" content="notranslate">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
@@ -17,7 +18,7 @@
         @vite(['resources/js/app.jsx'])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased notranslate">
         @inertia
     </body>
 </html>
